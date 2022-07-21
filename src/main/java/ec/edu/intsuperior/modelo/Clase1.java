9f7fe528fcbe0e17f0b0ac27124e5f0b7c0e3d72
user <user@DESKTOP-A4E8EPR>
2022-07-21 16:25:27 -0500

    Taller version 1.1 Prueba 1

---
+++ src/main/java/ec/edu/intsuperior/modelo/Clase1.java
@@ -0,0 +1,24 @@
+
+package ec.edu.intsuperior.modelo;
+
+public class Clase1 {
+    public static void metodoClase_1(){
+        System.out.println("metodo 1 de clase");
+    }
+    public static void metodoClase_2(){
+        System.out.println("metodo 2 de clase");
+    }
+     public static void metodoClase_3(){
+         System.out.println("metodo 3 de clase");
+    }
+     public static void metodoClase_4() {
+         System.out.println("metodo 4 de clase");
+    }
+     public static void metodoClase_5() {
+         System.out.println("metodo 5 de clase");
+    }
+
+    public void metodoInstancia_1() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ src/main/java/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,12 @@
+
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Clase1;
+public class Aplicacion {
+   public static void main(String[] args) {
+     Clase1 obj=new Clase1();
+     obj.metodoInstancia_1();
+   }
+}
+
+                
